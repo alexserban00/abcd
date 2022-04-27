9f5b0eb12cb0f0b532850df2781df23ec592cb40
alexserban00 <80423700+alexserban00@users.noreply.github.com>
2022-04-27 15:18:50 -0400

    Update loader.c

--- tema3/loader/loader.c
+++ tema3/loader/loader.c
@@ -10,6 +10,11 @@
 #include <signal.h>
 #include <fcntl.h>
 
+#include <errno.h>
+#include <math.h>
+#include <sys/mman.h>
+
+
 #include "exec_parser.h"
 
 static so_exec_t *exec;
@@ -18,8 +23,80 @@ static struct sigaction memsig;
 int fd;
 int sizePage;
 
-void execute_signal(int signalnum, siginfo_t *info, void *context) {
+static void execute_signal(int signum, siginfo_t *info, void *context)
+{
+	int i, page_found = 0, page_segment_index = -1;
+	void *mmap_ret;
+	int page_index, msync_ret;
 
+	for (i = 0; i < exec->segments_no; i++) {
+		if ((int) info->si_addr >= exec->segments[i].vaddr
+				&& (int) info->si_addr <= exec->segments[i].vaddr
+				+ exec->segments[i].mem_size) {
+			page_found = 1;
+			page_segment_index = i;
+			break;
+		}
+
+	}
+	if (page_found == 0) {
+		memsig.sa_sigaction(signum, info, context);
+		return;
+	}
+
+	uintptr_t vaddr = exec->segments[page_segment_index].vaddr;
+	unsigned int file_size = exec->segments[page_segment_index].file_size;
+	unsigned int mem_size = exec->segments[page_segment_index].mem_size;
+	unsigned int offset = exec->segments[page_segment_index].offset;
+	unsigned int perm = exec->segments[page_segment_index].perm;
+	uintptr_t addr = (uintptr_t) info->si_addr;
+	uintptr_t page_addr;
+
+	if (page_found == 1) {
+		page_index = floor((addr - vaddr) / sizePage);
+		page_addr = vaddr + (page_index * sizePage);
+		msync_ret = msync((int *) page_addr, sizePage, 0);
+		if (msync_ret == 0) {
+			memsig.sa_sigaction(signum, info, context);
+			return;
+		}
+		if (msync_ret == -1 && errno == ENOMEM) {
+			mmap_ret = mmap((void *) page_addr, sizePage, perm,
+					MAP_FIXED | MAP_PRIVATE, fd,
+					offset + page_index * sizePage);
+			if (mmap_ret == MAP_FAILED) {
+				memsig.sa_sigaction(signum, info, context);
+				return;
+			}
+			if (page_addr <= vaddr + file_size
+				&& page_addr + sizePage > vaddr + file_size
+				&& page_addr + sizePage <= vaddr + mem_size) {
+				unsigned int end = (unsigned int) sizePage - (vaddr + file_size - page_addr);
+
+				memset((void *) vaddr + file_size, 0, end);
+				return;
+			} else if (page_addr > vaddr + file_size
+				&& page_addr + sizePage < vaddr + mem_size) {
+
+				memset((void *) page_addr, 0, sizePage);
+				return;
+			} else if (page_addr > vaddr + file_size
+				&& page_addr < vaddr + mem_size
+				&& page_addr + sizePage >= vaddr + mem_size) {
+				unsigned int count = (unsigned int) ((vaddr + mem_size) - page_addr);
+
+				memset((void *) page_addr, 0, count);
+				return;
+			} else if (page_addr <= vaddr + file_size
+				&& page_addr + sizePage >= vaddr + mem_size) {
+
+				unsigned int count = (unsigned int) (mem_size - file_size);
+
+				memset((void *) vaddr + file_size, 0, count);
+				return;
+			}
+		}
+	}
 }
 
 
@@ -67,3 +144,4 @@ int so_execute(char *path, char *argv[])
 
 	return -1;
 }
+
