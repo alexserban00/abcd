8300a245d239660611d0c6f93550f3846fc80cca
alexserban00 <80423700+alexserban00@users.noreply.github.com>
2022-04-27 13:43:07 -0400

    Update loader.c

--- tema3/loader/loader.c
+++ tema3/loader/loader.c
@@ -1,126 +1,74 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <unistd.h>
-#include <errno.h>
-#include <math.h>
+#include <sys/mman.h>
 
 #include "exec_parser.h"
 
 static so_exec_t *exec;
 static struct sigaction old_action;
-int fd;
-int page_size;
+static int pageSize;
+static int fd;
 
 static void segv_handler(int signum, siginfo_t *info, void *context)
 {
-	int i, page_found = 0, page_segment_index = -1;
-	void *mmap_ret;
-	int page_index, msync_ret;
+    int i;
+    char *fault_addr, *base_addr, *page_addr, buffer[pageSize + 1];
 
-	for (i = 0; i < exec->segments_no; i++) {
-		if ((int) info->si_addr >= exec->segments[i].vaddr
-				&& (int) info->si_addr <= exec->segments[i].vaddr
-				+ exec->segments[i].mem_size) {
-			page_found = 1;
-			page_segment_index = i;
-			break;
-		}
+    if (signum != SIGSEGV) {
+        old_action.sa_sigaction(signum, info, context);
+        return;
+    }
 
-	}
-	if (page_found == 0) {
-		old_action.sa_sigaction(signum, info, context);
-		return;
-	}
+    fault_addr = (char *)info->si_addr;
+    base_addr = (char*)exec->base_addr;
+    unsigned long page_number = (fault_addr - base_addr) / pageSize;
+    page_addr = base_addr + page_number * pageSize;
 
-	uintptr_t vaddr = exec->segments[page_segment_index].vaddr;
-	unsigned int file_size = exec->segments[page_segment_index].file_size;
-	unsigned int mem_size = exec->segments[page_segment_index].mem_size;
-	unsigned int offset = exec->segments[page_segment_index].offset;
-	unsigned int perm = exec->segments[page_segment_index].perm;
-	uintptr_t addr = (uintptr_t) info->si_addr;
-	uintptr_t page_addr;
+    struct so_seg currentSegment;
+    for (i=0; i<exec->segments_no; i++) {
+        currentSegment = exec->segments[i];
+        if ((char*)currentSegment.vaddr <= page_addr &&
+            page_addr <= (char*)currentSegment.vaddr + currentSegment.mem_size) {
 
-	if (page_found == 1) {
-		page_index = floor((addr - vaddr) / page_size);
-		page_addr = vaddr + (page_index * page_size);
-		msync_ret = msync((int *) page_addr, page_size, 0);
-		if (msync_ret == 0) {
-			old_action.sa_sigaction(signum, info, context);
-			return;
-		}
-		if (msync_ret == -1 && errno == ENOMEM) {
-			mmap_ret = mmap((void *) page_addr, page_size, perm,
-					MAP_FIXED | MAP_PRIVATE, fd,
-					offset + page_index * page_size);
-			if (mmap_ret == MAP_FAILED) {
-				old_action.sa_sigaction(signum, info, context);
-				return;
-			}
-			if (page_addr <= vaddr + file_size
-				&& page_addr + page_size > vaddr + file_size
-				&& page_addr + page_size <= vaddr + mem_size) {
-				unsigned int end = (unsigned int) page_size - (vaddr + file_size - page_addr);
+            mmap(page_addr, (size_t)pageSize, currentSegment.perm, MAP_SHARED | MAP_ANONYMOUS | MAP_FIXED, fd, 0);
 
-				memset((void *) vaddr + file_size, 0, end);
-				return;
-			} else if (page_addr > vaddr + file_size
-				&& page_addr + page_size < vaddr + mem_size) {
+            lseek(fd, currentSegment.offset, SEEK_SET);
+            read(fd, buffer, (size_t)pageSize);
+            memcpy(page_addr, buffer, (size_t)pageSize);
+            return;
+        }
+    }
 
-				memset((void *) page_addr, 0, page_size);
-				return;
-			} else if (page_addr > vaddr + file_size
-				&& page_addr < vaddr + mem_size
-				&& page_addr + page_size >= vaddr + mem_size) {
-				unsigned int count = (unsigned int) ((vaddr + mem_size) - page_addr);
-
-				memset((void *) page_addr, 0, count);
-				return;
-			} else if (page_addr <= vaddr + file_size
-				&& page_addr + page_size >= vaddr + mem_size) {
-
-				unsigned int count = (unsigned int) (mem_size - file_size);
-
-				memset((void *) vaddr + file_size, 0, count);
-				return;
-			}
-		}
-	}
+    old_action.sa_sigaction(signum, info, context);
 }
 
-int so_init_loader(void)
+int so_init_loader(int fdd)
 {
-	//set signal
-	struct sigaction action;
-	int rc;
+    fd = fdd;
+    pageSize = getpagesize();
 
-	action.sa_sigaction = segv_handler;
-	sigemptyset(&action.sa_mask);
-	sigaddset(&action.sa_mask, SIGSEGV);
-	action.sa_flags = SA_SIGINFO;
+    struct sigaction action;
+    action.sa_sigaction = segv_handler;
+    sigemptyset(&action.sa_mask);
+    sigaddset(&action.sa_mask, SIGSEGV);
+    action.sa_flags = SA_SIGINFO;
 
-	rc = sigaction(SIGSEGV, &action, &old_action);
-	if (rc < 0)
-		return -1;
+    sigaction(SIGSEGV, &action, &old_action);
 
-	return -1;
+    return -1;
 }
 
 int so_execute(char *path, char *argv[])
 {
-	fd = open(path, O_RDONLY, 0644);
-	if (fd < 0)
-		return -1;
-	page_size = getpagesize();
+    exec = so_parse_exec(path);
+    if (!exec)
+        return -1;
 
-	exec = so_parse_exec(path);
-	if (!exec)
-		return -1;
+    so_start_exec(exec, argv);
 
-	so_start_exec(exec, argv);
-
-	return -1;
+    return -1;
 }
